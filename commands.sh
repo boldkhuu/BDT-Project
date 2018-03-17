8ecf1a9c1b5ffbdba60b2089c813e433f44cf370
Boldkhuu Batbaatar <hi@boldkhuu.com>
2018-03-16 22:36:53 -0500

    Useful shell commands

---
+++ commands.sh
@@ -0,0 +1,17 @@
+# starting zookeeper
+/usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties
+
+# starting kafka server
+/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
+
+# list kafka topics
+/usr/local/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
+
+# kafka console consumer
+/usr/local/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic twitter --from-beginning
+
+# run spark streaming
+/usr/local/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.1 spark/sparkStream.py 5
+
+# run kafka stream
+python kafka/twitter.py
